b8458ca1d4f9255768e05f57352099c3f830031b
Fernando Jesus Hernandez <fernandoj.hernandez123@gmail.com>
2023-09-30 13:50:23 -0400

    Adding 3rd and 4th challenges that were completed

---
+++ ReturnNegative.js
@@ -0,0 +1,12 @@
+function makeNegative(num) {
+    if (num >= 0) {
+        return -num;
+    }
+    else {
+        return num
+    }
+}
+
+console.log(makeNegative(-98));
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
