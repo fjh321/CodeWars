76cf33f40c19679abb99696a9aef59b68413ac09
Fernando Jesus Hernandez <64885403+fjh321@users.noreply.github.com>
2023-09-30 16:12:16 -0400

    Updated GrassHopper-PersonalizedMessage.js

--- Challenge2-(09-30-2023)/GrassHopper-PersonalizedMessage.js
+++ Challenge2-(09-30-2023)/GrassHopper-PersonalizedMessage.js
@@ -8,3 +8,12 @@ function greet(name, owner) {
 }
 
 //Grasshopper - Personalized Message//
+
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+// FUNDAMENTALSSTRINGS
