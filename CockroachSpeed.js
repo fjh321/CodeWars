2e8c798deab51822e8a80c12997f99f75e1f7835
Fernando Jesus Hernandez <fernandoj.hernandez123@gmail.com>
2023-10-06 11:29:37 -0400

    Adding challenge completed for Oct. 6th 2023

---
+++ CockroachSpeed.js
@@ -0,0 +1,9 @@
+function cockroachSpeed(s) {
+    let cmPerSec = Math.floor(s * (100000 / 3600))
+    return cmPerSec
+}
+console.log(3.56)
+
+
+//   The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
