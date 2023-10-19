f4364bebd474ea2be1e94c27699e8c0a8307590b
Fernando Jesus Hernandez <fernandoj.hernandez123@gmail.com>
2023-10-19 11:05:36 -0400

    adding codewars challenge for 10/19/2023

---
+++ upAndDownStringGrows.js
@@ -0,0 +1,12 @@
+const STRANGE_STRING = "ß"
+//trick was to find character in unicode that translated into multiple characters in English
+
+// Many programming languages provide the functionality of converting a string to uppercase or lowercase. For example, upcase/downcase in Ruby, upper/lower in Python, and toUpperCase/toLowerCase in Java/JavaScript, uppercase/lowercase in Kotlin. Typically, these methods won't change the size of the string.
+
+// For example, in Ruby, str.upcase.downcase.size == str.size is true for most cases.
+
+// However, in some special cases, the length of the transformed string may be longer than the original. Can you find a string that satisfies this criteria?
+
+// For example, in Ruby, That means str.upcase.downcase.size > str.size
+
+// You should just set the value of STRANGE_STRING to meet the previous criteria.
