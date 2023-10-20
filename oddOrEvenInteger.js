8df0552968d6092b9bbebb71d9af7480c6b69d64
Fernando Jesus Hernandez <fernandoj.hernandez123@gmail.com>
2023-10-20 13:50:40 -0400

    codewars challenge for 10/20/2023

---
+++ oddOrEvenInteger.js
@@ -0,0 +1,11 @@
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+        return 'Even'
+    } else if (number % 2 !== 0) {
+        return 'Odd'
+    }
+}
+
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
